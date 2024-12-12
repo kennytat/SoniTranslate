d7864da9805b24d6885a20b1f30487c07205413d
Kenny Tat <kennytat.app@gmail.com>
2024-12-12 09:49:08 +0700

    fix: celery.sh

--- celery.sh
+++ celery.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 celery -A tasks worker --pool=solo --loglevel=INFO -Q queue_stt,queue_diarization,queue_vtts,queue_xtts,queue_ptts &
-CUDA_VISIBLE_DEVICES=0 celery -A tasks worker --pool=solo --loglevel=INFO -Q queue_vtts,queue_xtts,queue_ptts &
-CUDA_VISIBLE_DEVICES=1 celery -A tasks worker --pool=solo --loglevel=INFO -Q queue_stt,queue_diarization,queue_vtts,queue_xtts,queue_ptts &
-CUDA_VISIBLE_DEVICES=1 celery -A tasks worker --pool=solo --loglevel=INFO -Q queue_vtts,queue_xtts,queue_ptts &
+device=$1
+CUDA_VISIBLE_DEVICES=$device celery -A tasks worker --pool=solo --loglevel=INFO -Q queue_stt,queue_diarization,queue_vtts,queue_xtts,queue_ptts &
+CUDA_VISIBLE_DEVICES=$device celery -A tasks worker --pool=solo --loglevel=INFO -Q queue_vtts,queue_xtts,queue_ptts &
